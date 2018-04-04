0a113cdaace30fd887e6898c298dbace7561dc23
Davor Heredia <davor.heredia@truextend.com>
2018-04-04 15:26:50 -0400

    Added TOOLARGENUMBER on ValidationResponses

--- src/com/company/ValidationResponses.java
+++ src/com/company/ValidationResponses.java
@@ -3,6 +3,7 @@ package com.company;
 public enum ValidationResponses {
     OK("Everything is OK"),
     NEGATIVENUMBER("The number is negative"),
+    TOOLARGENUMBER("The number is too large"),
     STRINGWITHNUMBERS("The string have numbers on it"),
     TOOLARGESTRING("The string is over 200 characters"),
     EMPTYSTRING("The string is empty");
