e411a38b87a134f66a224b96329129241c209dc4
Daniel Illanes <daniel.illanes@truextend.com>
2018-04-05 15:27:07 -0400

    Changing conditional numbers to constants

--- src/com/company/Validator.java
+++ src/com/company/Validator.java
@@ -2,14 +2,19 @@ package com.company;
 
 public class Validator {
 
+    static final int MIN_VALUE_FOR_NUMBER = 0;
+    static final int MAX_VALUE_FOR_NUMBER = 100;
+    static final int MAX_LENGHT_FOR_STRING = 200;
+    static final int MIN_ASCII_VALUE_FOR_NUMBER_CHARACTER_ = 48;
+    static final int MAX_ASCII_VALUE_FOR_NUMBER_CHARACTER = 57;
     public Validator(){
     }
 
     public static ValidationResponses Validate(int number){
-        if(number < 0){
+        if(number < MIN_VALUE_FOR_NUMBER){
             return ValidationResponses.NEGATIVENUMBER;
         }
-        if(number >= 100){
+        if(number >= MAX_VALUE_FOR_NUMBER){
             return ValidationResponses.TOOLARGENUMBER;
         }
         return ValidationResponses.OK;
@@ -19,7 +24,7 @@ public class Validator {
         if(str.isEmpty()){
             return ValidationResponses.EMPTYSTRING;
         }
-        if(str.length() > 200){
+        if(str.length() > MAX_LENGHT_FOR_STRING){
             return ValidationResponses.TOOLARGESTRING;
         }
         for (int i=0; i< str.length();i++){
@@ -31,7 +36,7 @@ public class Validator {
     }
 
     public static boolean isNumber(char s) {
-        if(s >= 48 && s <= 57)
+        if(s >= MIN_ASCII_VALUE_FOR_NUMBER_CHARACTER_ && s <= MAX_ASCII_VALUE_FOR_NUMBER_CHARACTER)
             return true;
         return false;
     }
